16dcc4425527344a00537976719b772d49e2ea0a
tbellicha <tanguy.bellicha@epitech.eu>
2024-05-07 19:06:56 -0700

    [ADD] Terraform files for GCP

---
+++ google/main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.19.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("service_account.json")
+  project     = "MY_PROJECT_ID"
+
+}
+
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "instance-20240311-232644" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-20240311-232644"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20240213"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-standard-8"
+  name         = "instance-20240311-232644"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/MY_PROJECT_ID/regions/asia-northeast1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "MY_CLIENT-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  metadata = {
+    startup-script = <<SCRIPT
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y sysbench
+    SCRIPT
+  }
+
+  # sysbench --test=cpu run && sysbench --test=memory run && sysbench --test=fileio --file-test-mode=seqwr run
+
+  zone = "asia-northeast1-a"
+}
